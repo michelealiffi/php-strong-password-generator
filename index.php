1b9d2c9939b4a89c0c2eff29bd5e23a7e378c4bd
michelealiffi <michelealiffi890@gmail.com>
2024-06-19 13:48:00 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:32,height:32/https://www.filepicker.io/api/file/iperH9IBTIalnkREa5pL" rel="icon" type="image/png">
+    <title>PHP Strong Password Generator</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
